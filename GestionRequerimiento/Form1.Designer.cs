f7114be6ad2325b95f9bdaa2981479000223ebcb
Cazzia <cazzia.14@gmail.com>
2017-10-15 12:19:02 -0500

    Agregar archivos de proyecto.

---
+++ GestionRequerimiento/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace GestionRequerimiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.cboUsuarioCreacion = new System.Windows.Forms.ComboBox();
+            this.cboUsuarioResponsable = new System.Windows.Forms.ComboBox();
+            this.cboEstado = new System.Windows.Forms.ComboBox();
+            this.dtpFechaCreacion = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaSolucion = new System.Windows.Forms.DateTimePicker();
+            this.cboSeveridad = new System.Windows.Forms.ComboBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de Requerimientos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descripcion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Usuario Creacion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Usuario Responsable";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Estado";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(418, 161);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Categoria";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(418, 95);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Fecha Solución";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(418, 62);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Fecha Creación";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(418, 128);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Severidad";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(73, 50);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(325, 20);
+            this.txtNombre.TabIndex = 10;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(92, 81);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(306, 20);
+            this.txtDescripcion.TabIndex = 11;
+            // 
+            // cboUsuarioCreacion
+            // 
+            this.cboUsuarioCreacion.FormattingEnabled = true;
+            this.cboUsuarioCreacion.Location = new System.Drawing.Point(138, 108);
+            this.cboUsuarioCreacion.Name = "cboUsuarioCreacion";
+            this.cboUsuarioCreacion.Size = new System.Drawing.Size(179, 21);
+            this.cboUsuarioCreacion.TabIndex = 19;
+            // 
+            // cboUsuarioResponsable
+            // 
+            this.cboUsuarioResponsable.FormattingEnabled = true;
+            this.cboUsuarioResponsable.Location = new System.Drawing.Point(137, 146);
+            this.cboUsuarioResponsable.Name = "cboUsuarioResponsable";
+            this.cboUsuarioResponsable.Size = new System.Drawing.Size(179, 21);
+            this.cboUsuarioResponsable.TabIndex = 20;
+            // 
+            // cboEstado
+            // 
+            this.cboEstado.FormattingEnabled = true;
+            this.cboEstado.Location = new System.Drawing.Point(138, 182);
+            this.cboEstado.Name = "cboEstado";
+            this.cboEstado.Size = new System.Drawing.Size(179, 21);
+            this.cboEstado.TabIndex = 21;
+            // 
+            // dtpFechaCreacion
+            // 
+            this.dtpFechaCreacion.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaCreacion.Location = new System.Drawing.Point(505, 60);
+            this.dtpFechaCreacion.Name = "dtpFechaCreacion";
+            this.dtpFechaCreacion.Size = new System.Drawing.Size(109, 20);
+            this.dtpFechaCreacion.TabIndex = 22;
+            // 
+            // dtpFechaSolucion
+            // 
+            this.dtpFechaSolucion.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaSolucion.Location = new System.Drawing.Point(505, 91);
+            this.dtpFechaSolucion.Name = "dtpFechaSolucion";
+            this.dtpFechaSolucion.Size = new System.Drawing.Size(109, 20);
+            this.dtpFechaSolucion.TabIndex = 23;
+            // 
+            // cboSeveridad
+            // 
+            this.cboSeveridad.FormattingEnabled = true;
+            this.cboSeveridad.Location = new System.Drawing.Point(505, 122);
+            this.cboSeveridad.Name = "cboSeveridad";
+            this.cboSeveridad.Size = new System.Drawing.Size(164, 21);
+            this.cboSeveridad.TabIndex = 24;
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(505, 154);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(164, 21);
+            this.cboCategoria.TabIndex = 25;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(246, 223);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(253, 23);
+            this.btnGuardar.TabIndex = 26;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 272);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.cboCategoria);
+            this.Controls.Add(this.cboSeveridad);
+            this.Controls.Add(this.dtpFechaSolucion);
+            this.Controls.Add(this.dtpFechaCreacion);
+            this.Controls.Add(this.cboEstado);
+            this.Controls.Add(this.cboUsuarioResponsable);
+            this.Controls.Add(this.cboUsuarioCreacion);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.ComboBox cboUsuarioCreacion;
+        private System.Windows.Forms.ComboBox cboUsuarioResponsable;
+        private System.Windows.Forms.ComboBox cboEstado;
+        private System.Windows.Forms.DateTimePicker dtpFechaCreacion;
+        private System.Windows.Forms.DateTimePicker dtpFechaSolucion;
+        private System.Windows.Forms.ComboBox cboSeveridad;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ GestionRequerimiento/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GestionRequerimiento.Properties;
+using AccesoDatos;
+
+namespace GestionRequerimiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            GestionRequerimientoEntities db = new GestionRequerimientoEntities();
+
+            cboUsuarioCreacion.DataSource = db.Usuarios.ToList();
+            cboUsuarioCreacion.ValueMember = "Id";
+            cboUsuarioCreacion.DisplayMember = "Nombres";
+
+
+            cboUsuarioResponsable.DataSource = db.Usuarios.ToList();
+            cboUsuarioResponsable.ValueMember = "Id";
+            cboUsuarioResponsable.DisplayMember = "Nombres";
+
+            cboEstado.DataSource = db.Estados.ToList();
+            cboEstado.ValueMember = "Id";
+            cboEstado.DisplayMember = "Nombre";
+
+            cboSeveridad.DataSource = db.Severidad.ToList();
+            cboSeveridad.ValueMember = "Id";
+            cboSeveridad.DisplayMember = "Nombre";
+
+            cboCategoria.DataSource = db.Categoria.ToList();
+            cboCategoria.ValueMember = "Id";
+            cboCategoria.DisplayMember = "Nombre";
+
+
+
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                GestionRequerimientoEntities db = new GestionRequerimientoEntities();
+                Requerimiento requerimiento = new Requerimiento();
+
+                String nombre = txtNombre.Text;
+                String descripcion = txtDescripcion.Text;
+                int usuarioCreacion = int.Parse(cboUsuarioCreacion.SelectedValue.ToString());
+                int usuarioResponsable = int.Parse(cboUsuarioResponsable.SelectedValue.ToString());
+                int estado = int.Parse(cboEstado.SelectedValue.ToString());
+                int severidad = int.Parse(cboSeveridad.SelectedValue.ToString());
+                int categoria = int.Parse(cboCategoria.SelectedValue.ToString());
+                DateTime fechaCreacion = dtpFechaCreacion.Value;
+                DateTime fechaSolucion = dtpFechaSolucion.Value;
+
+                requerimiento.NombreRequerimiento = nombre;
+                requerimiento.Descripcion = descripcion;
+                requerimiento.UsuarioCreacion = usuarioCreacion;
+                requerimiento.UsuarioResponsable = usuarioResponsable;
+                requerimiento.Estado = estado;
+                requerimiento.Severidad = severidad;
+                requerimiento.Categoria = categoria;
+                requerimiento.FechaCreacion = fechaCreacion;
+                requerimiento.FechaSolucion = fechaSolucion;
+
+                db.Requerimiento.Add(requerimiento);
+                db.SaveChanges();
+
+                MessageBox.Show("El requerimiento se guardo correctamente");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("ERROR : " );
+            }
+
+            
+
+
+
+        }
+
+
+
+
+        }
+    }
+
